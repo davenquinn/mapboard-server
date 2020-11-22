e013c7cebfec78a5f200d69051422d4cf47ff640
Daven Quinn <dev@davenquinn.com>
2020-11-22 12:57:14 -0500

    Downgraded socket io server

--- src/topology-watcher.ts
+++ src/topology-watcher.ts
@@ -1,10 +1,16 @@
 import socketIO from "socket.io";
 
-export async function topologyWatcher(db, server) {
+export async function topologyWatcher(db, server, socketOptions = {}) {
   console.log("Starting topology watcher");
   // Watches for PG events
-  const io = socketIO(server);
-  io.on("connection", () => console.log("Client connected"));
+  const io = socketIO(server, socketOptions);
+  io.on("connection", (socket: any) => {
+    console.log("Client connected");
+    console.log(socket.handshake);
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
+    });
+  });
 
   // Listen for data
   const conn = await db.connect({ direct: true });
@@ -12,6 +18,7 @@ export async function topologyWatcher(db, server) {
     const data = JSON.parse(message.payload);
     io.emit("topology", data);
     const v = JSON.stringify(data, null, 2);
+    console.log("\n");
     console.log(`Emitting topology change event: ${v}`);
     // The payload isn't used for anything right now...
   });
